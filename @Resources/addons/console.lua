7951fa10d27e67751e418c7f4aa4a0740e9d085e
MininMobile <miningamer@gmail.com>
2018-05-06 19:24:45 +0100

    it works not yes

--- @Resources/addons/console.lua
+++ @Resources/addons/console.lua
@@ -1,3 +1,11 @@
 function updateOutput(input)
-	print(input)
+	output = SKIN:GetMeter("output")
+	text = output:GetOption("Text", "")
+
+	if (text ~= "") then
+		text = text .. "\r\n"
+	end
+
+	result = "I peed my pants!"
+	SKIN:Bang("!SetOption", "output", "text", text .. result)
 end
