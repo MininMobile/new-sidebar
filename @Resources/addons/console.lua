8434fa381be275ac816a1c2a6a4963d67342eb35
MininMobile <miningamer@gmail.com>
2018-05-07 00:41:06 +0100

    Add More Shit

--- @Resources/addons/console.lua
+++ @Resources/addons/console.lua
@@ -16,23 +16,54 @@ function updateOutput(input)
 	if (command[1] == "echo") then
 		table.remove(command, 1);
 		result = table.concat(command, " ")
-	elseif (command[1] == "task") and (command[2] == "add") then
-		tasks = SKIN:GetVariable("tasks")
-
-		table.remove(command, 1);
-		table.remove(command, 1);
-		task = table.concat(command, " ")
-
-		if (tasks == "No Tasks Available") then
+	elseif (command[1] == "task") then
+		if (command[2] == "add") then
+			tasks = SKIN:GetVariable("tasks")
+	
+			table.remove(command, 1);
+			table.remove(command, 1);
+			task = table.concat(command, " ")
+	
+			if (tasks == "No Tasks Available") then
+				settings = SKIN:ReplaceVariables("#@#settings.inc")
+				SKIN:Bang("!WriteKeyValue", "Variables", "tasks", task, settings)
+			else
+				settings = SKIN:ReplaceVariables("#@#settings.inc")
+				newtasks = tasks .. "," .. task
+				SKIN:Bang("!WriteKeyValue", "Variables", "tasks", newtasks, settings)
+			end
+	
+			result = "Added [" .. task .. "] to tasklist"
+		elseif (command[2] == "remove") then
 			settings = SKIN:ReplaceVariables("#@#settings.inc")
-			SKIN:Bang("!WriteKeyValue", "Variables", "tasks", task, settings)
+			tasks = SKIN:GetVariable("tasks")
+
+			tasklist = splitComma(tasks)
+			taskamount = tableLength(tasklist)
+
+			if (tasks == "No Tasks Available") then
+				result = "ERROR: No Tasks to Remove"
+			elseif (tonumber(command[3]) > taskamount) or (tonumber(command[3]) <= 0) then
+				result = "ERROR: Task Doesn't Exist"
+			else
+				table.remove(tasklist, tonumber(command[3]))
+				newtasks = table.concat(tasklist, ",")
+
+				if (newtasks == "") then
+					newtasks = "No Tasks Available"
+				end
+
+				SKIN:Bang("!WriteKeyValue", "Variables", "tasks", newtasks, settings)
+
+				result = "Removed #" .. command[3] .. " from tasklist"
+			end
+		elseif (command[2] == "clear") then
+			settings = SKIN:ReplaceVariables("#@#settings.inc")
+			SKIN:Bang("!WriteKeyValue", "Variables", "tasks", "No Tasks Available", settings)
+			result = "Chlorinated the taskpool"
 		else
-			settings = SKIN:ReplaceVariables("#@#settings.inc")
-			newtasks = tasks .. "," .. task
-			SKIN:Bang("!WriteKeyValue", "Variables", "tasks", newtasks, settings)
-		end
-
-		result = "Added [" .. task .. "] to tasklist"
+			result = "ERROR: Invalid Argument [" .. command[2] .. "]"
+		end		
 	else
 		result = "ERROR: Invalid Command [" .. command[1] .. "]"
 	end
@@ -41,11 +72,31 @@ function updateOutput(input)
 end
 
 function split(str)
-	t = { }
+	local t = { }
 
 	for i in string.gmatch(str, "%S+") do
 		table.insert( t, i )
 	end
 
 	return t
+end
+
+function splitComma(str)
+	local t = { }
+
+	for i in string.gmatch(str, "([^,]+),?") do
+		table.insert( t, i )
+	end
+
+	return t
+end
+
+function tableLength(t)
+	local count = 0
+
+	for _ in pairs(t) do
+		count = count + 1
+	end
+
+	return count
 end
