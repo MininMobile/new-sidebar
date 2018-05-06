42d3d51f74cb15e0ae7b37203917af3210d429a8
MininMobile <miningamer@gmail.com>
2018-05-06 20:01:48 +0100

    add echo command

--- @Resources/addons/console.lua
+++ @Resources/addons/console.lua
@@ -6,6 +6,29 @@ function updateOutput(input)
 		text = text .. "\r\n"
 	end
 
-	result = "I peed my pants!"
+	if (input == "") then
+		return
+	end
+
+	result = ""
+	command = split(input)
+
+	if (command[1] == "echo") then
+		table.remove(command, 1);
+		result = table.concat(command, " ")
+	else
+		result = "ERROR: Invalid Command [" .. command[1] .. "]"
+	end
+
 	SKIN:Bang("!SetOption", "output", "text", text .. result)
+end
+
+function split(str)
+	t = { }
+
+	for i in string.gmatch(str, "%S+") do
+		table.insert( t, i )
+	end
+
+	return t
 end
